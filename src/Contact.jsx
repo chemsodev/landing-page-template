c9334aa38e3cdd682506a9cb4decd7046d0349aa
recay26 <okba.moulahoum@gmail.com>
2024-10-02 18:33:05 +0100

    ho ho

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -6,10 +6,10 @@ function Contact (){
         <div className="contact">
             <h1>Contact me</h1>
             <div className='buttons'>
-                <a href="tel:+213541520675">
+                <a href="tel:+213xxxxxxxx">
                 <button><img src="src/assets/phone-regular-24.png"/>Appeler</button>
                 </a>
-                <a href="https://wa.me/2130551063100">
+                <a href="https://wa.me/213xxxxxxxxx">
                 <button><img src="src/assets/whatsapp-logo-24.png"/>Whatsapp</button>
                 </a>
             </div>
--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ function Header (){
 
     return(
         <header>
-            <p><a href="tel:+213541520675">0567890923 إضغط هنا للإتصال</a></p>
+            <p><a href="tel:+213xxxxxxxx">05xxxxxxx إضغط هنا للإتصال</a></p>
         </header>
     )
 }
